59d008e667656733077abef54b3b99db52be1aad
Kim Mäderer <paul.maederer@page-one.de>
2019-08-20 16:20:03 +0200

    Added missing .wav file to resources

--- learnLatin/Properties/Resources.Designer.cs
+++ learnLatin/Properties/Resources.Designer.cs
@@ -71,6 +71,15 @@ namespace learnLatin.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream Imperium {
+            get {
+                return ResourceManager.GetStream("Imperium", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap roman_city {
